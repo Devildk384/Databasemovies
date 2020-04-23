16102910f10249489c374d8d9b8901719cb24ccb
Devildk384 <durgeshkumar384@gmail.com>
2020-04-23 15:20:03 +0530

    Updates

---
+++ precache-manifest.149851bae9c601d350db7fa8dbe4a886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca901d494d89a1d4ef1efe6bd16412b",
+    "url": "/Devildk384/databasemovies/index.html"
+  },
+  {
+    "revision": "254dd7106852e88ca12c",
+    "url": "/Devildk384/databasemovies/static/css/main.6300cd14.chunk.css"
+  },
+  {
+    "revision": "81b542771719bf458a54",
+    "url": "/Devildk384/databasemovies/static/js/2.63b4aaba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Devildk384/databasemovies/static/js/2.63b4aaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254dd7106852e88ca12c",
+    "url": "/Devildk384/databasemovies/static/js/main.962d5f5c.chunk.js"
+  },
+  {
+    "revision": "0f4937f91774f2c1c6d5",
+    "url": "/Devildk384/databasemovies/static/js/runtime-main.db4ff37a.js"
+  }
+]);
